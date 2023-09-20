67ed9af2e91cff68a54818a9ab2baa56f844d8dd
francescomascellino <franesco.mascellino@gmail.com>
2023-09-20 16:20:09 +0200

    add async lifehook  to render component

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,22 +14,30 @@ createApp({
             // ARRAY DELLE EMAIL GENERATE
             rndEmails: [],
 
+            listFull: false,
+
         }
     },
 
-    mounted() {
+    //I LIFEHOOKS POSSONO ESSERE ASINCRONI
+    async mounted() {
 
         for (let i = 0; i < 10; i++) {
 
             //API PER GENERARE INDIRIZZI EMAIL RANDOMICI
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            //PRIMA DI PROCEDERE ASPETTA CHE TUTTE LE CHIAMATE DI azios VENGANO COMPIUTE
+            await axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
 
                 //GET RANDOM EMAIL DALL'API .then .push() IN rndEmails
                 .then(response => {
                     this.rndEmails.push(response.data.response);
                 });
 
-        }
+        };
+
+        // SOLO DOPO listFull DIVENTA TRUE (LA LISTA E' PIENA PERCHE' HA FATTO DIECI GIRI DEL FOR LOOP)
+        this.listFull = true;
+
 
     },
 
