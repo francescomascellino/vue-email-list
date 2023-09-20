8bb8dbf2b417e0c40fb81335b753c0dcc960b32e
francescomascellino <franesco.mascellino@gmail.com>
2023-09-20 13:16:34 +0200

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+        mounted() {
+
+
+
+        },
+
+    }
+
+}).mount('#app')
